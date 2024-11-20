33ce578ce983187c3197ef8a3904e0d39423deb2
Aarti Itikirala <aartiitikirala@aartis-mbp.mynetworksettings.com>
2024-11-20 16:01:26 -0500

    favorites show up on home page, but not with all info

--- lib/pages/home/home_tab.dart
+++ lib/pages/home/home_tab.dart
@@ -1,16 +1,303 @@
-// home_tab.dart
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomeTab extends StatelessWidget {
+class HomeTab extends StatefulWidget {
   const HomeTab({Key? key}) : super(key: key);
 
   @override
+  _HomeTabState createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
+  List<Map<String, dynamic>> favorites = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchFavorites();
+  }
+
+  Future<void> fetchFavorites() async {
+    final user = FirebaseAuth.instance.currentUser;
+
+    if (user == null) {
+      print('User not logged in!');
+      setState(() {
+        isLoading = false;
+      });
+      return;
+    }
+
+    final userId = user.uid;
+    final userDoc = FirebaseFirestore.instance.collection('users').doc(userId);
+
+    try {
+      final snapshot = await userDoc.get();
+      if (snapshot.exists) {
+        final fetchedFavorites = List<Map<String, dynamic>>.from(
+          snapshot.data()?['favorites'] ?? [],
+        );
+        setState(() {
+          favorites = fetchedFavorites;
+          isLoading = false;
+        });
+      } else {
+        setState(() {
+          isLoading = false;
+        });
+      }
+    } catch (e) {
+      print('Error fetching favorites: $e');
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  void showDetailsDialog(BuildContext context, Map<String, dynamic> place) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: Colors.transparent,
+      builder: (BuildContext context) {
+        return Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).scaffoldBackgroundColor,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                place['name'] ?? 'Unknown Name',
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 16),
+              if (place['photoUrl'] != null)
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(
+                    place['photoUrl'],
+                    width: double.infinity,
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              const SizedBox(height: 16),
+              if (place['vicinity'] != null)
+                Row(
+                  children: [
+                    const Icon(Icons.location_pin, color: Colors.blue),
+                    const SizedBox(width: 8),
+                    Expanded(
+                      child: Text(
+                        place['vicinity'],
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ),
+              if (place['phone'] != null)
+                Row(
+                  children: [
+                    const Icon(Icons.phone, color: Colors.blue),
+                    const SizedBox(width: 8),
+                    Text(
+                      place['phone'],
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+              if (place['rating'] != null)
+                Row(
+                  children: [
+                    const Icon(Icons.star, color: Colors.yellow),
+                    const SizedBox(width: 8),
+                    Text(
+                      place['rating'].toString(),
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+              if (place['hours'] != null)
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Opening Hours:',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      ...List<Widget>.from(
+                        (place['hours'] as List<dynamic>).map(
+                          (hour) => Text(
+                            hour,
+                            style: const TextStyle(fontSize: 14),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Close'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Home Tab Here',
-        style: TextStyle(fontSize: 24),
-      ),
+    return Column(
+      children: [
+        const Expanded(
+          child: Center(
+            child: Text(
+              'Home Tab Here',
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+        ),
+        Container(
+          alignment: Alignment.centerLeft,
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: const Text(
+            'Favorites',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Expanded(
+          child: isLoading
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : favorites.isNotEmpty
+                  ? ListView.builder(
+                      itemCount: favorites.length,
+                      itemBuilder: (context, index) {
+                        final favorite = favorites[index];
+                        final photoUrl = favorite['photoUrl'];
+                        final rating = favorite['rating'];
+
+                        return InkWell(
+                          onTap: () {
+                            showDetailsDialog(context, favorite);
+                          },
+                          child: Card(
+                            margin: const EdgeInsets.symmetric(
+                                horizontal: 16, vertical: 8),
+                            elevation: 6,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(
+                                  6.0, 16.0, 16.0, 16.0),
+                              child: Row(
+                                children: [
+                                  IconButton(
+                                    icon: Icon(
+                                      Icons.favorite,
+                                      color: Colors.red,
+                                    ),
+                                    onPressed: () {
+                                      // Optional: Add logic to remove from favorites
+                                      print(
+                                          'Remove ${favorite['name']} from favorites');
+                                    },
+                                  ),
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          favorite['name'] ?? 'No Name',
+                                          style: const TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        const SizedBox(height: 4),
+                                        Text(
+                                          favorite['vicinity'] ?? 'No Address',
+                                          style: const TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.grey,
+                                          ),
+                                        ),
+                                        const SizedBox(height: 8),
+                                        Row(
+                                          children: [
+                                            Icon(Icons.star,
+                                                color: Colors.yellow[700],
+                                                size: 16),
+                                            const SizedBox(width: 4),
+                                            Text(
+                                              rating != null
+                                                  ? rating.toString()
+                                                  : 'No Rating',
+                                              style:
+                                                  const TextStyle(fontSize: 14),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  if (photoUrl != null)
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 8.0),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(10),
+                                        child: Image.network(
+                                          photoUrl,
+                                          width: 100,
+                                          height: 100,
+                                          fit: BoxFit.cover,
+                                          errorBuilder:
+                                              (context, error, stackTrace) {
+                                            return const Icon(
+                                                Icons.broken_image,
+                                                size: 100);
+                                          },
+                                        ),
+                                      ),
+                                    ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    )
+                  : const Center(
+                      child: Text(
+                        'No favorites yet!',
+                        style: TextStyle(fontSize: 16, color: Colors.grey),
+                      ),
+                    ),
+        ),
+      ],
     );
   }
 }
